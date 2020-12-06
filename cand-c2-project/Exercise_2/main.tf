2f0a8a5ce45f647f99d2e6ffa441cafd725c3be5
hh_gymcraft <h.hildebrandt@gymcraft.es>
2020-12-06 11:56:15 +0100

    Changes according to project review

--- cand-c2-project/Exercise_2/main.tf
+++ cand-c2-project/Exercise_2/main.tf
@@ -4,6 +4,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "lambda_function_name" {
+  default = "my_udacity_lambda"
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -26,7 +30,7 @@ EOF
 
 resource "aws_lambda_function" "udacity_lambda" {
   filename      = "greet_lambda.zip"
-  function_name = "my_udacity_lambda"
+  function_name = "${var.lambda_function_name}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "greet_lambda.lambda_handler"
   runtime = "python3.6"
@@ -36,4 +40,42 @@ resource "aws_lambda_function" "udacity_lambda" {
       greeting = "Hello Udacity!"
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
